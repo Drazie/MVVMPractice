223a8d211dd828b5621bd6bbd381c867e529dc45
Drazie <54319841+Drazie@users.noreply.github.com>
2021-03-02 01:08:52 +0200

    LandingPage connected to MainSections model

---
+++ MVVMPractice/Model/MainSection.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MVVMPractice.Model
+{
+    public class MainSection
+    {
+        public string SectionName { get; set; }
+    }
+}
--- MVVMPractice/ViewModel/LandingViewModel.cs
+++ MVVMPractice/ViewModel/LandingViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using MVVMPractice.Model;
+using MVVMPractice.View;
 using Xamarin.Forms;
 
 namespace MVVMPractice.ViewModel
@@ -8,19 +11,60 @@ namespace MVVMPractice.ViewModel
     {
         public LandingViewModel()
         {
+            Sections = GetMainSections();
         }
-        ObservableCollection<Button> buttons;
-        public ObservableCollection<Button> Buttons
+
+        ObservableCollection<MainSection> sections;
+        public ObservableCollection<MainSection> Sections
         {
             get
             {
-                return buttons;
+                return sections;
             }
             set
             {
-                buttons = value;
-                
+                sections = value;
             }
+        }
+
+        private MainSection selectedSection;
+        public MainSection SelectedSection
+        {
+            get
+            {
+                return selectedSection;
+            }
+            set
+            {
+                selectedSection = value;
+            }
+        }
+
+        public Command SelectionCommand => new Command(DisplaySection);
+
+        private void DisplaySection()
+        {
+            if(selectedSection != null)
+            {
+                var viewModel = new SectionViewModel();
+                var sectionPage = new SectionPage();
+
+                var navigation = Application.Current.MainPage as NavigationPage;
+                navigation.PushAsync(sectionPage, true);
+            }
+        }
+
+        private ObservableCollection<MainSection> GetMainSections()
+        {
+            return new ObservableCollection<MainSection>
+            {
+                new MainSection{ SectionName = "General" },
+                new MainSection{ SectionName = "Projects" },
+                new MainSection{ SectionName = "Skills" },
+                new MainSection{ SectionName = "Experience" },
+                new MainSection{ SectionName = "Goals" },
+                new MainSection{ SectionName = "Contact" }
+            };
         }
     }
 }
